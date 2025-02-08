f74ad2bfe3ff992525c28f2b937fa25d5f49e1e7
Sagar <sagarbhati8837@gmail.com>
2025-02-08 16:22:17 +0530

    Encapsulate the game logic into a function

--- snake_water_gun_game.py
+++ snake_water_gun_game.py
@@ -13,40 +13,45 @@ import random
 0 for gun
 '''
 
-# Computer randomly chooses between snake, water, and gun
-computer = random.choice([1, -1, 0])
+def play_game():
+    # Computer randomly chooses between snake, water, and gun
+    computer = random.choice([1, -1, 0])
 
-# User input for choice
-your_choice = input("Enter your choice (s for snake, w for water, g for gun): ")
+    # User input for choice
+    your_choice = input("Enter your choice (s for snake, w for water, g for gun): ")
 
-# Dictionary to map user input to game choices
-you_Dict = {"s": 1, "w": -1, "g": 0}
+    # Dictionary to map user input to game choices
+    you_Dict = {"s": 1, "w": -1, "g": 0}
 
-# Dictionary to map game choices to their corresponding strings
-reverse_dict = {1: "Snake", -1: "Water", 0: "Gun"}
+    # Dictionary to map game choices to their corresponding strings
+    reverse_dict = {1: "Snake", -1: "Water", 0: "Gun"}
 
-# Get the user's choice based on input
-you = you_Dict[your_choice]
+    # Get the user's choice based on input
+    you = you_Dict[your_choice]
 
-# Print choices made by user and computer
-print(f"You chose: {reverse_dict[you]}\nComputer chose: {reverse_dict[computer]}")
+    # Print choices made by user and computer
+    print(f"You chose: {reverse_dict[you]}\nComputer chose: {reverse_dict[computer]}")
 
-# Check if it's a draw
-if computer == you:  # when computer and user both choose the same
-    print("It's a draw")
-else:
-    # Determine the outcome based on the rules of the game
-    if computer == 1 and you == -1:
-        print("You lose!")
-    elif computer == -1 and you == 1:
-        print("You won!")
-    elif computer == 0 and you == 1:
-        print("You lose!")
-    elif computer == 1 and you == 0:
-        print("You won!")
-    elif computer == -1 and you == 0:
-        print("You lose!")
-    elif computer == 0 and you == -1:
-        print("You won!")
+    # Check if it's a draw
+    if computer == you:  # when computer and user both choose the same
+        print("It's a draw")
     else:
-        print("Something went wrong")
+        # Determine the outcome based on the rules of the game
+        if computer == 1 and you == -1:
+            print("You lose!")
+        elif computer == -1 and you == 1:
+            print("You won!")
+        elif computer == 0 and you == 1:
+            print("You lose!")
+        elif computer == 1 and you == 0:
+            print("You won!")
+        elif computer == -1 and you == 0:
+            print("You lose!")
+        elif computer == 0 and you == -1:
+            print("You won!")
+        else:
+            print("Something went wrong")
+
+# Call the function to play the game
+if __name__ == "__main__":
+    play_game()
